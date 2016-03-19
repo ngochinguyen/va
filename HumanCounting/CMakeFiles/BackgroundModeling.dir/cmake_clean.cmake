cbb3671a5c5b9779d35f4eaa4cdae2ff9f22c3d9
ngochinguyen <ngochinguyen@gmail.com>
2016-03-19 20:57:03 +0700

    Del FaceReg

---
+++ HumanCounting/CMakeFiles/BackgroundModeling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/BackgroundModeling.dir/bs.cpp.o"
+  "BackgroundModeling.pdb"
+  "BackgroundModeling"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/BackgroundModeling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
